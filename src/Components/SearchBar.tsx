87eeead02967c3b27570e3da273d92991d03c7ae
Ivan Heng Ze Hao <ivanhengzehao@gmail.com>
2021-12-20 18:59:06 +0800

    master - Searchbar is now a class component + stateful saving of search inputs + installed axios

--- src/Components/SearchBar.tsx
+++ src/Components/SearchBar.tsx
@@ -1,18 +1,51 @@
+import * as React from 'react';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 
-export default function SearchBar() {
-  return (
-    <Autocomplete
-      id="SearchBar"
-      freeSolo
-      fullWidth
-      autoHighlight
-      size = 'small'
-      options={autoCompleteOptions.map((option) => option.title)}
-      renderInput={(params) => <TextField {...params} label="Search stocks" />}
+interface Props {
+
+}
+
+interface State {
+  searchTerm: String | null
+}
+
+class SearchBar extends React.Component<Props, State> {
+
+  constructor(props: Props) {
+    super(props);
+    this.state = { searchTerm: null };
+    this.onSearchValueChange = this.onSearchValueChange.bind(this);
+  }
+  
+  onSearchValueChange = (event: React.SyntheticEvent<Element, Event>, value: String | null) => {
+    // console.log(event.currentTarget);
+    console.log("Searched term = " + value);
+    
+    this.setState({ searchTerm: value });
+  }
+
+  componentDidUpdate(prevProps: Props, prevState: State) {
+    console.log("prevProps:" + JSON.stringify(prevProps));
+    console.log("prevState:" + JSON.stringify(prevState));
+    console.log("Props:" + JSON.stringify(this.props));
+    console.log("State:" + JSON.stringify(this.state));
+  }
+
+  render() {
+    return (
+      <Autocomplete
+        id="SearchBar"
+        freeSolo
+        fullWidth
+        autoHighlight
+        size = 'small'
+        options={autoCompleteOptions.map((option) => option.title)}
+        renderInput={(params) => <TextField {...params} label="Search stocks" />}
+        onChange={this.onSearchValueChange}
     />
-  );
+    );
+  }
 }
 
 // TODO: True autocomplete data retrieval
@@ -21,5 +54,7 @@ const autoCompleteOptions = [
   { title: 'Ethereum', year: 1972 },
   { title: 'Polygon', year: 1974 },
   { title: 'Shiba', year: 2008 },
-  { title: 'Dogecoine', year: 1957 },
+  { title: 'Dogecoin', year: 1957 },
 ];
+
+export default SearchBar;
