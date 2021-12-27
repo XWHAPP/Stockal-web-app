3ce7ac27a6510be697a36e7a33e4dcbe19626d65
Ivan Heng Ze Hao <ivanhengzehao@gmail.com>
2021-12-27 17:11:15 +0800

    master - fix mockURL

--- src/Enums.ts
+++ src/Enums.ts
@@ -1,4 +1,4 @@
 export enum url {
-  mock = 'http://stockal.mocklab.io/v1/Sentiment',
+  mock = 'http://stockal.mocklab.io/',
   actual = 'http://stockalsentimentanalysistest4-env.eba-ppz6bznz.ap-southeast-1.elasticbeanstalk.com',
 }
