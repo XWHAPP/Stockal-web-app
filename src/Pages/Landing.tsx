a9781a6212200aa5ccc1029e3118523fcdfe5bca
Ivan Heng Ze Hao <ivanhengzehao@gmail.com>
2021-12-30 15:06:37 +0800

    revamp - Header component now directly on Landing

--- src/Pages/Landing.tsx
+++ src/Pages/Landing.tsx
@@ -1,5 +1,12 @@
 import * as React from 'react';
-import Header from '../Components/Header';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchBar from '../Components/SearchBar';
 
 interface Props {}
 
@@ -19,7 +26,22 @@ class Landing extends React.Component<Props, State> {
   render() {
     return (
       <div>
-        <Header />
+        {/* Header */}
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position='sticky'>
+            <Toolbar>
+              <IconButton size='large' edge='start' color='inherit' aria-label='menu' sx={{ mr: 2 }}>
+                <MenuIcon />
+              </IconButton>
+              <Typography variant='h4' component='div' sx={{ flexGrow: 1, mr: 2 }}>
+                Stockal
+              </Typography>
+              <SearchBar></SearchBar>
+              <Button color='inherit'>Login</Button>
+            </Toolbar>
+          </AppBar>
+        </Box>
+        {/* //// */}
       </div>
     );
   }
