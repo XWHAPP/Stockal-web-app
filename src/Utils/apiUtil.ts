ef0d51d4d7d7435cd7be695496b645ecfa123161
Ivan Heng Ze Hao <ivanhengzehao@gmail.com>
2022-01-04 22:22:13 +0800

    integration - update actual API

--- src/Utils/apiUtil.ts
+++ src/Utils/apiUtil.ts
@@ -3,4 +3,8 @@ import { URL } from '../Enums';
 
 export const configureAxios = (isMock: boolean) => {
   axios.defaults.baseURL = isMock ? URL.MOCK : URL.ACTUAL;
+
+  // if (!isMock) {
+  //   axios.defaults.headers.common['Origin'] = 'asdsad';
+  // }
 };
