a56d68e6945e3ea2931f1bcf50bdab760ce375bd
Ivan Heng Ze Hao <ivanhengzehao@gmail.com>
2022-01-06 17:21:00 +0800

    integration - change to env_5

--- src/Enums.ts
+++ src/Enums.ts
@@ -1,6 +1,7 @@
 export enum URL {
   MOCK = 'https://stockal.mocklab.io/',
-  ACTUAL = 'https://cors-everywhere.herokuapp.com/http://stockalsentimentanalysistest4-env.eba-ppz6bznz.ap-southeast-1.elasticbeanstalk.com',
+  ACTUAL_4 = 'https://cors-everywhere.herokuapp.com/http://stockalsentimentanalysistest4-env.eba-ppz6bznz.ap-southeast-1.elasticbeanstalk.com',
+  ACTUAL_5 = 'https://cors-everywhere.herokuapp.com/http://stockalsentimentanalysistest5-env.eba-3npacdag.ap-southeast-1.elasticbeanstalk.com',
 }
 
 export enum SENTIMENT {
--- src/Utils/apiUtil.ts
+++ src/Utils/apiUtil.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { URL } from '../Enums';
 
 export const configureAxios = (isMock: boolean) => {
-  axios.defaults.baseURL = isMock ? URL.MOCK : URL.ACTUAL;
+  axios.defaults.baseURL = isMock ? URL.MOCK : URL.ACTUAL_5;
 
   // if (!isMock) {
   //   axios.defaults.headers.common['Origin'] = 'asdsad';
